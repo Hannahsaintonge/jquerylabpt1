a998f6e12a1bb8201d64f636027c2570b14c9300
hannahsaintonge <hannahsaintonge@gmail.com>
2018-05-08 08:14:07 -0400

    first commit for jquerylab

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+$(document).ready(() => {
+
+    let btn = $("#table_button");
+
+    btn.click(() => {
+      $(".reservation_form").toggle();
+
+});
+     let exit = $(".material-icons");
+
+exit.click(() => {
+    $(".reservation_form").toggle();
+
+});
+
+
+let exitSave = $(".save_button");
+
+exitSave.click(() => {
+    $(".reservation_form").toggle();
+
+});
+
+
+
+
+});
